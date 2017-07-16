3ff6a745c6fe4b914dfa459615121afd8a128649
Stephen Jantz <sjantz@hotmail.de>
2017-07-17 00:54:22 +0200

    Change the color of the navigationbar of MenuViewController

--- Taschenrechner/MenuViewController.swift
+++ Taschenrechner/MenuViewController.swift
@@ -11,68 +11,77 @@ import UIKit
 class MenuViewController: UIViewController {
     
     let inputsContainerView: UIView = {
-       
+        
         let view = UIView()
-        view.backgroundColor = UIColor.white
         view.translatesAutoresizingMaskIntoConstraints = false
 
         return view
     }()
     
+    let buttonRound: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.brown
+        button.tintColor = UIColor.blue
+        
+        return button
+    }()
     
-    
-
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
-        
+
         setNaviBar()
         
-
-
+    }
+    
+    func button(){
+        
     }
     
     //add the navigationBar
     func setNaviBar(){
+        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+
         let screenSize: CGRect = UIScreen.main.bounds
-        
         let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screenSize.width , height: 80))
-  
         
         let navItem = UINavigationItem(title: "Verlauf")
         
-        let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: #selector(done))
-        navItem.rightBarButtonItem = doneItem
+        
+        
+        let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.cancel, target: nil, action: #selector(done))
+        navItem.leftBarButtonItem = doneItem
         navBar.setItems([navItem], animated: false)
         self.view.addSubview(navBar)
-        
-
-        
+        UINavigationBar.appearance().tintColor = UIColor.white
+        navBar.barTintColor = UIColor.black
+        navBar.isTranslucent = false
     }
     
     //methode muss noch definiert werden
     func done(){
         
+        
+        
+        //self.navigationController?.popViewController(animated: true)
+        
     }
- 
-//    
-//    func backAction(){
-//        self.navigationController?.popViewController(animated: true)
-//    }
     
-
+    //
+    //    func backAction(){
+    //        self.navigationController?.popViewController(animated: true)
+    //    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     
-//    //change the color of the statutsbar to white
-//    override var preferredStatusBarStyle: UIStatusBarStyle{
-//        return .lightContent
-//    }
+    //change the color of the statutsbar to white
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
     
 }
 extension UIColor{
