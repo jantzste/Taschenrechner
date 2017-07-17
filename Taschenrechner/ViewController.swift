a4aaad921843109695553c52a5e0d88b8b01d211
Stephen Jantz <sjantz@hotmail.de>
2017-07-17 13:20:16 +0200

    add the divideButton and sumButton into ViewController

--- Taschenrechner/ViewController.swift
+++ Taschenrechner/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
         setNaviBar()
         plusButton()
         minusButton()
+        divideButton()
+        sumButton()
     
     }
     
@@ -56,7 +58,7 @@ class ViewController: UIViewController {
     
     func plusButton (){
     
-        let btn: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
+        let btn: UIButton = UIButton(frame: CGRect(x: 16, y: 400, width: 80, height: 50))
         btn.backgroundColor = UIColor.black
         btn.setTitle("+", for: UIControlState.normal)
         btn.addTarget(self, action: #selector(plusButtonAction), for: UIControlEvents.touchUpInside)
@@ -68,7 +70,7 @@ class ViewController: UIViewController {
         var btnsendtag: UIButton = sender
         if btnsendtag.tag == 1{
             //do anything here
-            print("gedrückt")
+            print("plus gedrückt")
         }else{
             print("nicht gedrückt")
         }
@@ -76,7 +78,7 @@ class ViewController: UIViewController {
     
     func minusButton (){
         
-        let btn: UIButton = UIButton(frame: CGRect(x: 210, y: 400, width: 100, height: 50))
+        let btn: UIButton = UIButton(frame: CGRect(x: 116, y: 400, width: 80, height: 50))
         btn.backgroundColor = UIColor.black
         btn.setTitle("-", for: UIControlState.normal)
         btn.addTarget(self, action: #selector(minusButtonAction), for: UIControlEvents.touchUpInside)
@@ -88,12 +90,52 @@ class ViewController: UIViewController {
         var btnsendtag: UIButton = sender
         if btnsendtag.tag == 1{
             //do anything here
-            print("gedrückt")
+            print("minus gedrückt")
         }else{
             print("nicht gedrückt")
         }
     }
     
+    
+    func divideButton (){
+        
+        let btn: UIButton = UIButton(frame: CGRect(x: 216, y: 400, width: 80, height: 50))
+        btn.backgroundColor = UIColor.black
+        btn.setTitle("/", for: UIControlState.normal)
+        btn.addTarget(self, action: #selector(divideButtonAction), for: UIControlEvents.touchUpInside)
+        btn.tag = 1
+        self.view.addSubview(btn)
+    }
+    
+    func divideButtonAction(sender: UIButton!){
+        var btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1{
+            //do anything here
+            print("geteilt gedrückt")
+        }else{
+            print("nicht gedrückt")
+        }
+    }
+    
+    func sumButton (){
+        
+        let btn: UIButton = UIButton(frame: CGRect(x: 316, y: 400, width: 80, height: 50))
+        btn.backgroundColor = UIColor.black
+        btn.setTitle("*", for: UIControlState.normal)
+        btn.addTarget(self, action: #selector(sumButtonAction), for: UIControlEvents.touchUpInside)
+        btn.tag = 1
+        self.view.addSubview(btn)
+    }
+    
+    func sumButtonAction(sender: UIButton!){
+        var btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1{
+            //do anything here
+            print("mal gedrückt")
+        }else{
+            print("nicht gedrückt")
+        }
+    }
 
     
 
