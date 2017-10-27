4ed6103b2288f1de07d155e01c943f710e3622b5
Stephen Jantz <sjantz@hotmail.de>
2017-10-27 13:36:46 +0200

    code cleaning for the sidebar

--- Taschenrechner/ViewControllerContainer.swift
+++ Taschenrechner/ViewControllerContainer.swift
@@ -8,26 +8,42 @@
 
 import UIKit
 
-class ViewControllerContainer: UIViewController {
+class ViewControllerContainer: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let table:tableViewContent = tableViewContent()
+    var tableData:NSArray = ["first","second"]
+    
+    var tableView:UITableView!
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setNaviBar()
-        
-        table.setupTableView()
-        
-        self.view.addSubview(table.tableView)
 
+        setupTableView()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        table.setupTableView()
+    
+    func setupTableView(){
         
-        self.view.addSubview(table.tableView)
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // tableView = UITableView(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight -  barHeight))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "sidebarCell")
+        tableView.delegate      =   self
+        tableView.dataSource    =   self
+        
+        //set backgroundColor of the ViewController
+        tableView.backgroundColor = UIColor(r: 246, g: 246, b: 246)
+        tableView.separatorStyle = .none
+        //have to use this to add data to the table
+        self.view.addSubview(tableView)
     }
+    
+
 
 //    func setLogo(){
 //
@@ -47,6 +63,38 @@ class ViewControllerContainer: UIViewController {
 //
 //    }
 //
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "sidebarCell"
+        
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: cellIdentifier)
+            
+            let tableData = self.tableData[indexPath.row]
+            cell?.textLabel!.text = "test"
+            
+            
+        }
+
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
    
     
     func setNaviBar(){
--- Taschenrechner/tableViewContent.swift
+++ Taschenrechner/tableViewContent.swift
@@ -8,61 +8,8 @@
 
 import UIKit
 
-class tableViewContent: UITableView, UITableViewDelegate, UITableViewDataSource{
-    
-    
-    var tableData = [
-        ["name":"Item1","image":"food"], ["name":"Item2","image":"GreenBackground"],["name":"Item3","image":"QRCode"]
-    ]
-    
-     var tableView: UITableView = UITableView()
-    
-    
-    //initialize and set tableView variable
-    func setupTableView(){
-        
-        tableView = UITableView(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
-        tableView.delegate      =   self
-        tableView.dataSource    =   self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "sidebarCell")
-       
-        //set backgroundColor of the ViewController
-        tableView.backgroundColor = UIColor(r: 246, g: 246, b: 246)
-        tableView.separatorStyle = .none
-        
-    }
-    
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cellIdentifier = "sidebarCell"
-        
-        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        
-        
-        if cell == nil {
-            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: cellIdentifier)
-            
-            let tableData = self.tableData[indexPath.row]
-            cell?.textLabel?.text = tableData["name"]!
-            cell?.textLabel?.textColor = UIColor.black
-            cell?.imageView?.image = UIImage(named: tableData["image"]!)
-            
-        }
-        return cell!
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
+class tableViewContent: UITableView{
+
 
 }
 
